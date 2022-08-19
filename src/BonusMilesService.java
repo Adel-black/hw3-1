4342b6dd7f6e5fb19ba68639bb25b46d588d6b11
Alina Trifonova <trifonova.alina@mail.ru>
2022-08-19 12:04:43 +1000

    all

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
-    public int calculate(int price) {
-        int cost = price / 20;
-        return cost;
+    public int calculate(int cost) {
+        int bonusMiles = 20;
+        return cost / bonusMiles;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        int price = 12000;
         int miles = service.calculate(price);
-        System.out.println(miles);
+        System.out.println("На вашем счету бонусных миль: " + miles);
     }
 }
